0fce04b6a7f98a4297a7248cec017280f6568ca5
Aritra Chakraborty <aritracodm2002@kgpian.iitkgp.ac.in>
2022-02-22 22:18:43 +0530

    Moved camera to signup.java

--- app/src/main/java/com/example/attendance/SignUp.java
+++ app/src/main/java/com/example/attendance/SignUp.java
@@ -1,14 +1,57 @@
 package com.example.attendance;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class SignUp extends AppCompatActivity {
 
+    Button button;
+    ImageView imageview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        button=findViewById(R.id.capture);
+        imageview=findViewById(R.id.imageView2);
+        if(ContextCompat.checkSelfPermission(SignUp.this,
+                Manifest.permission.CAMERA)!= PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(SignUp.this,
+                    new String[]{
+                            Manifest.permission.CAMERA
+                    },
+                    100);
+        }
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent,100);
+
+
+            }
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 100) {
+            Bitmap captureImage;
+            assert data != null;
+            captureImage = (Bitmap) data.getExtras().get("data");
+            imageview.setImageBitmap(captureImage);
+        }
     }
 }
